1c94f86d0a5b39676aa2dd0788a4b547e3e131c2
Toby Inkster <mail@tobyinkster.co.uk>
2025-12-15 17:32:31 +0000

    bump version; changelog

--- lib/Marlin/Attribute.pm
+++ lib/Marlin/Attribute.pm
@@ -5,7 +5,7 @@ use warnings;
 package Marlin::Attribute;
 
 our $AUTHORITY = 'cpan:TOBYINK';
-our $VERSION   = '0.002005';
+our $VERSION   = '0.003000';
 
 use parent 'Sub::Accessor::Small';
 use B ();
