7c4f558d67bb0baa1b8959c5d7787cc2cc4d08ba
Toby Inkster <mail@tobyinkster.co.uk>
2025-12-15 09:11:12 +0000

    bump version; changelog

--- lib/Marlin/Attribute.pm
+++ lib/Marlin/Attribute.pm
@@ -5,7 +5,7 @@ use warnings;
 package Marlin::Attribute;
 
 our $AUTHORITY = 'cpan:TOBYINK';
-our $VERSION   = '0.002003';
+our $VERSION   = '0.002004';
 
 use parent 'Sub::Accessor::Small';
 use B ();
--- lib/Marlin/Util.pm
+++ lib/Marlin/Util.pm
@@ -5,7 +5,7 @@ use warnings;
 package Marlin::Util;
 
 our $AUTHORITY = 'cpan:TOBYINK';
-our $VERSION   = '0.002003';
+our $VERSION   = '0.002004';
 
 use parent 'Exporter::Tiny';
 
