b1d43d320461d6668eb619897f92397d024a444e
Toby Inkster <mail@tobyinkster.co.uk>
2026-01-07 20:32:58 +0000

    misc changes

--- lib/Marlin/XAttribute/Lvalue.pm
+++ lib/Marlin/XAttribute/Lvalue.pm
@@ -5,7 +5,7 @@ use warnings;
 package Marlin::XAttribute::Lvalue;
 
 our $AUTHORITY = 'cpan:TOBYINK';
-our $VERSION   = '0.010000';
+our $VERSION   = '0.010001';
 
 use attributes ();
 use Class::XSAccessor ();
